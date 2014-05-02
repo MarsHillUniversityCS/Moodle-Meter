c4a2571fe07ff74673d96706d239d38ad3c83be4
Carter Benge <carterbenge@gmail.com>
2014-05-01 20:45:56 -0400

    Initial commit. Includes graph in moodle page.

---
+++ user_graph.php
@@ -0,0 +1,68 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This block will display a summary of hours and earnings for the worker.
+ *
+ * @package    Moodle Meter
+ * @copyright  Marty Gilbert & Carter Benge
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ */
+
+require_once(dirname(__FILE__) . '/../../config.php');
+require_once('lib.php');
+
+require_login();
+
+$courseid = required_param('id', PARAM_INT);
+$userid = optional_param('userid', 0, PARAM_INT);
+
+$urlparams['id'] = $courseid;
+$urlparams['userid'] = $userid;
+
+$course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
+
+$PAGE->set_course($course);
+$context = $PAGE->context;
+
+$isteacher = false;
+if (has_capability('moodle/grade:viewall', $context)) { //teacher
+    $isteacher = true;
+    $urlparams['userid'] = 0;
+}
+
+
+if(!$isteacher && $userid == 0){
+    print_error('improper permissions');
+}
+
+$index = new moodle_url($CFG->wwwroot.'/blocks/meter/user_graph.php', $urlparams);
+
+$strtitle = get_string('pluginname','block_meter');
+
+$PAGE->set_url($index);
+$PAGE->set_title($strtitle);
+$PAGE->set_heading($strtitle);
+$PAGE->set_pagelayout('base');
+
+echo $OUTPUT->header();
+
+$imageurl = new moodle_url('/blocks/meter/graph.php', array('id'=>$courseid, 'userid'=>$userid));
+$graph = html_writer::empty_tag('img', array('src' => $imageurl, 'alt'=>'Moodle Meter Graph'));
+echo html_writer::tag('div', $graph, array('class' => 'graph'));
+
+echo $OUTPUT->footer();
