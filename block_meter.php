de19a6d8fc879c0d50cb1d175409b9f6740b8c24
Marty Gilbert <mgilbert@mhc.edu>
2014-05-02 13:36:57 -0400

    Added config code

--- block_meter.php
+++ block_meter.php
@@ -30,33 +30,83 @@ class block_meter extends block_base {
 
     function init() {
         global $USER;
-        if(is_siteadmin($USER->id)){
-            $this->title = get_string('blocktitle', 'block_meter');
-        }
+        $this->title = get_string('blocktitle', 'block_meter');
     }
 
     function get_content() {
-        //global $CFG, $DB, $USER, $OUTPUT, $COURSE;
-        global $COURSE;
+        global $COURSE, $CFG, $USER, $OUTPUT, $SESSION;
         if($this->content !== NULL){
             return $this->content;
         }
 
+
         $this->content = new stdClass;
 
+        if(!isset($this->config)){
+
+            $configurl = new moodle_url($CFG->wwwroot.
+                '/course/view.php', array('id'=>$COURSE->id, 'sesskey'=>sesskey(),
+                'bui_editid'=>$this->instance->id));
+            
+            $this->content->text .= $OUTPUT->action_link($configurl, get_string('noconfigpresent', 'block_meter'));
+
+            return $this->content->text;
+
+        }
+
+
+        /*
         $this->content->text    .= '<h4>The Moodle Meter Block</h4>';
         $this->content->text    .= '<br />';//.$COURSE->id;
+        */
+        $graphurl = new moodle_url($CFG->wwwroot.'/blocks/meter/user_graph.php',
+            array('id'=>$COURSE->id));
 
-        //do_stats_run($COURSE->id);
         if(has_capability('moodle/grade:viewall', $this->context)){
-            //$this->content->text    .= '<br />You are a teacher';
-            $students = get_student_stats($COURSE->id);
-            foreach ($students as $student){
-                $this->content->text .= $student->lastname.', '.$student->firstname.
-                    ' - Level '.$student->level.'<br />';
+            $students = get_all_student_stats($COURSE->id);
+            if(!$students){
+                $this->content->text .= 'No activity data present. Please try again in 24 hours';
+                return $this->content->text;
             }
+
+            foreach ($students as $student){
+                
+                $graphurl->params(array('userid'=>$student->userid));
+
+                $this->content->text .= 
+                    html_writer::empty_tag('img', 
+                    array('src' => $CFG->wwwroot.'/blocks/meter/pix/level'.
+                    $student->level.'circle.png', 'class'=>'icon'));
+
+                $this->content->text .= $OUTPUT->action_link($graphurl, 
+                    $student->lastname.', '.$student->firstname).'<br />';
+            }
+
+            $graphurl->remove_params('userid');
+            $this->content->text .= '<br /><p style="text-align: right;">'.$OUTPUT->action_link($graphurl, 
+                get_string('viewallusers', 'block_meter')).'</p>';
+
         } else if(has_capability('mod/assignment:submit', $this->context)){
-            $this->content->text    .= '<br />You are a student';
+
+            $level = get_student_stats($USER->id, $COURSE->id);
+
+        
+            /*
+            $this->content->text .= 
+                html_writer::empty_tag('img', 
+                array('src' => $CFG->wwwroot.'/blocks/meter/pix/level'.$level.'.png',
+                    'alt'=>get_string('viewgraph', 'block_meter')));
+            */
+
+
+            
+            $graphurl->params(array('userid'=>$USER->id));
+            $this->content->text .= $OUTPUT->action_icon($graphurl,
+                        new pix_icon('level'.$level, get_string('viewgraph', 'block_meter'),
+                        'block_meter'));
+
+            $this->content->text .= '<br />';
+            $this->content->text .= get_string('level'.$level.'user', 'block_meter');
         }
 
         return $this->content;
@@ -91,5 +141,32 @@ class block_meter extends block_base {
         $DB->set_field('block', 'lastcron', strtotime("Today 6am"),
             array('name'=>'meter'));
     }
+
+    /**
+    * Override the instance_config_save method
+    */
+    function instance_config_save($data, $nolongerused = false){
+        parent::instance_config_save($data, $nolongerused);
+        global $DB, $COURSE;
+        foreach ($data as $name => $value){
+
+            $rec = $DB->get_record('block_meter_config',
+                array('courseid'=>$COURSE->id,'name'=>$name));
+
+            $conf = new stdClass;
+            $conf->name = $name;
+            $conf->value = $value;
+            $conf->courseid= $COURSE->id;
+
+            if($rec){
+                $conf->id = $rec->id;
+                $DB->update_record('block_meter_config',$conf);
+            } else {
+                $DB->insert_record('block_meter_config',$conf);
+            }
+
+        }
+    }
+
 
 }
