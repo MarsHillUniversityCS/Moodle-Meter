cf2e87cd7a669a18c8965511d1fbcbad016efca2
Marty Gilbert <mgilbert@mhc.edu>
2014-05-02 13:33:43 -0400

    Moved this procedure to a function in lib

--- batch/batch_load_historical_stats_data.php
+++ batch/batch_load_historical_stats_data.php
@@ -5,19 +5,4 @@ require_once('../../../config.php');
 require_once('../lib.php');
 
 
-global $DB, $USER, $CFG;
-
-$start = 1377316800; //august 24th @ midnight (2013)
-$end = strtotime("+1 day", $start);
-
-while($end < 1388466000){ //Dec 31 @ midnight (2013)
-    //echo $start.' '.$end."\n";
-    do_stats_run(7, $start, $end); 
-
-    
-    //$start = $end + 1; //00:00:00
-    $end = strtotime("+1 day", $end); //23:59:59
-
-    echo "Processed from ".$start.' to '.$end."\n";
-}
-
+load_historical_data(7, 0, 1388527171, true);
