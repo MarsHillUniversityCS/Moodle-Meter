834704735322b2c283288114401e0489b070a421
Marty Gilbert <mgilbert@mhc.edu>
2014-05-08 11:50:16 -0400

    Initial import

---
+++ settings.php
@@ -0,0 +1,31 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+
+    $options = array();
+    for ($i = 0; $i < 24; $i++) {
+        $options[$i] = sprintf("%02d",$i);
+    }
+
+    $settings->add(
+        new admin_setting_configselect('block_meter_cronhour', 
+        get_string('cronhour', 'block_meter'),
+        get_string('cronhourdesc', 'block_meter'), 4, $options));
+
+
+    foreach(range(1,6) as $i){
+        $settings->add(
+            new admin_setting_configtext('block_meter_tier'.$i.'_weight', 
+            get_string('tier'.$i, 'block_meter'),
+            get_string('tier'.$i.'desc', 'block_meter'), 35-(($i-1)*5), PARAM_INT));
+    }
+
+    $settings->add(
+        new admin_setting_configtext('block_meter_default_weight',
+        get_string('defaultweight', 'block_meter'), 
+        get_string('defaultweightdesc', 'block_meter'), 1, PARAM_INT));
+    
+
+}
