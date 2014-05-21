90ecdda10865eeeed34a1be76a9a24cd7746fb9a
Marty Gilbert <mgilbert@mhc.edu>
2014-05-21 10:56:26 -0400

    Misc changes

--- batch/batch_load_historical_stats_data.php
+++ batch/batch_load_historical_stats_data.php
@@ -5,5 +5,7 @@ require_once('../../../config.php');
 require_once('../lib.php');
 
 
-load_historical_data(7, 0, 1388527171, true);
+//load_historical_data(7, 0, 1388527171, true);
+//load_historical_data(9, 0, 1388527171, true);
+load_historical_data(11, 0, 1388527171, true);
 //load_historical_data(8, 0, 1388527171, true);
