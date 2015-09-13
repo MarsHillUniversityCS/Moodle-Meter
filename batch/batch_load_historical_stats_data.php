5f3f4bbc09e1d98b773f5ae82f7fb9be1dc58a52
Marty Gilbert <martygilbert@gmail.com>
2015-09-13 08:15:05 -0400

    Added new batch to re-calc all stats

--- batch/batch_load_historical_stats_data.php
+++ batch/batch_load_historical_stats_data.php
@@ -5,7 +5,23 @@ require_once('../../../config.php');
 require_once('../lib.php');
 
 
-//load_historical_data(7, 0, 1388527171, true);
-//load_historical_data(9, 0, 1388527171, true);
-load_historical_data(11, 0, 1388527171, true);
-//load_historical_data(8, 0, 1388527171, true);
+/**
+
+This batch script will reset *all* courses, and re-calculate the historical stats.
+
+Run this at a time of low system load; it'll cause the server to crunch
+numbers for a good while.
+
+*/
+
+
+$metercourses = $DB->get_records('block_meter_stats', null, '', 'courseid');
+
+echo 'Processing '.sizeof($metercourses).' courses'."\n";
+
+if(!$metercourses) exit;
+
+foreach ($metercourses as $course){
+    //echo $course->courseid."\n";
+    load_historical_data($course->courseid, 0, 0, true);
+}
