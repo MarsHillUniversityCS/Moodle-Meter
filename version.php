640953bde191232db49dfedd2a113aa412e537b8
marty <martygilbert@gmail.com>
2015-10-04 10:44:57 -0400

    changed version number

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2015052702';
+$plugin->version = '2015100427';
 $plugin->requires  = 2014050900;        // Requires this Moodle version
 $plugin->cron = 86400; // Set min time between cron executions to  1 day
